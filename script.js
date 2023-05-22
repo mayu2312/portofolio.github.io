ed47952171c4e342fe023932984a5cb64b1cdea2
Yudha Maulana <131224899+mayu2312@users.noreply.github.com>
2023-05-22 11:19:39 +0700

    Update Script JS Error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
 
@@ -8,15 +8,4 @@ themeToggle.addEventListener('change', function() {
     } else {
       body.classList.remove('darkMode');
     }
-=======
-const themeToggle = document.getElementById('themeToggle');
-const body = document.body;
-
-themeToggle.addEventListener('change', function() {
-    if (this.checked) {
-      body.classList.add('darkMode');
-    } else {
-      body.classList.remove('darkMode');
-    }
->>>>>>> 0b409b34cb4d31bc33b5e2698b133f0203821dad
   });
